aa510592b8a36f0324aba7d297c143ed37d09528
codediodeio <hello@jeffdelaney.me>
2019-04-02 20:36:49 -0700

    full working demo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Episode 177 - Flutter Flare Demo
+
+Create a clone of the Giphy mobile app bottom navigation bar from scratch. 
+
+![flutter flare giphy clone](https://firebasestorage.googleapis.com/v0/b/fireship-app.appspot.com/o/assets%2F177-flare%2Fflutter-flare-demo.gif?alt=media&token=3aaed583-c37b-49dc-8b35-f5532ceb8487)
+
+```
+git clone
+flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          body: Container(color: Colors.black, child: Center(child: NavBar()))),
+    );
+  }
+}
+
+class MenuItem {
+  final String name;
+  final Color color;
+  final double x;
+  MenuItem({this.name, this.color, this.x});
+}
+
+class NavBar extends StatefulWidget {
+  createState() => NavBarState();
+}
+
+class NavBarState extends State<NavBar> {
+  List items = [
+    MenuItem(x: -1.0, name: 'house', color: Colors.lightBlue[100]),
+    MenuItem(x: -0.5, name: 'planet', color: Colors.purple),
+    MenuItem(x: 0.0, name: 'camera', color: Colors.greenAccent),
+    MenuItem(x: 0.5, name: 'heart', color: Colors.pink),
+    MenuItem(x: 1.0, name: 'head', color: Colors.yellow),
+  ];
+
+  MenuItem active;
+
+  @override
+  void initState() {
+    super.initState();
+
+    active = items[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    return Container(
+      height: 80,
+      color: Colors.black,
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            alignment: Alignment(active.x, -1),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 1000),
+              height: 8,
+              width: w * 0.2,
+              color: active.color,
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: items.map((k) {
+                return _flare(k);
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _flare(MenuItem item) {
+    return GestureDetector(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: FlareActor(
+            'assets/${item.name}.flr',
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            animation: item.name == active.name ? 'go' : 'idle',
+          ),
+        ),
+      ),
+      onTap: () {
+        setState(() {
+          active = item;
+        });
+      },
+    );
+  }
+}
